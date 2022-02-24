0587827bd537726ba914a38acebd2429040846b2
conradbuys11 <conradbuys11@gmail.com>
2022-02-24 15:35:18 -0500

    worked on DAO & impl, learned new annotations

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	//inject dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("list")
 	public String listCustomers(Model model) {
+		
+		//get stuff from DAO
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		//add customers to model
+		//first arg is name in model, second arg is the value we're passing in
+		model.addAttribute("customers", customers);
 		
 		return "list-customers";
 	}
